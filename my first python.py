66114e895a2eeb031b368eb1a773cb3338360cd0
SuicidezSheep <altheradrian@gmail.com>
2024-01-25 03:50:21 +0800

    Python is Kind

---
+++ my first python.py
@@ -0,0 +1,17 @@
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = num1 + num2
+print(num1, "+", num2," = ", result)
+
+x = 10;
+y = 15;
+
+result = x > y
+print(result) #x is not greater than y so FALSE
+
+result = x < y
+print(result) #x is indeed less than y so TRUE
+
+print("Hello, World!")
